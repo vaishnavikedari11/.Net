11debf54e8a754ebe00e138a39a0d0a80c357fea
Kedari Vaishnavi Kailas <116325133+vaishnavikedari11@users.noreply.github.com>
2024-02-04 19:09:33 +0530

    Add files via upload

---
+++ CSharp/TypesOfConstructors/CopyConstructor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor11
+{
+
+    internal class CopyConstructor
+    {
+        int x;
+        public CopyConstructor(int i) {
+           x = i;
+        }
+        //copy constructor -
+        public CopyConstructor(CopyConstructor obj1 ) { //here CopyConstructor is new data-type
+            x=obj1.x;   //x is lical variable
+        }
+        public void display()
+        {
+            Console.WriteLine("Value of x = "+x);
+        }
+        static void Main(string[] args)
+        {
+            CopyConstructor obj1=new CopyConstructor(1);
+            obj1.display();
+            CopyConstructor obj2=new CopyConstructor(obj1);
+            obj2.display();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp/TypesOfConstructors/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor11
+{
+    internal class Program
+    {
+        int i;
+        string s;
+        bool b;
+        public Program()//Implicitly defined constructor= Default constructor= parameter less constructor
+        {
+            i = 0;
+            s = null;
+            b= false;
+        }
+        static void Main(string[] args)
+        {
+            Program obj1 = new Program();
+            Console.WriteLine("implicit Constructor is called..");
+            Console.WriteLine(obj1.i);
+            Console.WriteLine(obj1.s);
+            Console.WriteLine(obj1.b);
+            Console.ReadLine();
+        }
+    }
+}
