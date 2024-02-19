1333f23282da129dfcd3221450f038beccc58b48
Kedari Vaishnavi Kailas <116325133+vaishnavikedari11@users.noreply.github.com>
2024-02-19 17:55:10 +0530

    Add files via upload

---
+++ InterrfaceProject/InterrfaceProject/Interface1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterrfaceProject
+{
+   public interface Interface1
+    {
+       void Add(int a,int b);
+    }
+    public interface Interface2 : Interface1
+    {
+       void Sub(int a,int b);
+    }
+    public class Interface3 : Interface2
+    {
+        //while declaring method we make them as public so we use public or Interface name.method name()
+
+
+        void Interface1.Add(int a, int b)
+        {
+            Console.WriteLine("Addition = " + (a + b));
+        }
+
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine("Substraction = " + (a - b));
+        }
+
+
+        static void Main()
+        {
+            Interface3 i3 = new Interface3();
+            //
+           // i3.Add(1,2);
+            i3.Sub(1,2);
+            //
+            Interface2 i2 = i3;
+            i2.Add(11,22);
+            i2.Sub(12,32);
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ InterrfaceProject/InterrfaceProject/MyClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterrfaceProject
+{
+    public interface Interface5
+    {
+        void Test();
+        void Show();
+    }
+    public interface Interface6:Interface5
+    {
+        void Test();
+        void Show();
+    }
+
+    public class MyClass : Interface5, Interface6
+    {
+        //1st way of defining method
+        public void Test() {
+            Console.WriteLine("Test() is called ");
+        }
+        //2nd way of defining method
+        void Interface5.Show() 
+        {
+            Console.WriteLine("Show() is called by using Interface5");
+        }
+
+        void Interface6.Show() {
+            Console.WriteLine("Show() is called by using Interface6");
+        }
+
+        static void Main()
+        {
+            MyClass m1=new MyClass();
+            m1.Test();
+            //if we have to call show() that particular class then we call only by using that class referenvce
+            Interface5 i1 = m1;
+            i1.Show();
+
+            Interface6 i2 = m1;
+            i2.Show();
+
+            Console.ReadLine();
+        }
+    }
+}
