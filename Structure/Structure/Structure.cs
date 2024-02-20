25a7749d586ada2f0bb37aead6e299886f53badc
Kedari Vaishnavi Kailas <116325133+vaishnavikedari11@users.noreply.github.com>
2024-02-20 16:12:41 +0530

    Add files via upload

---
+++ Structure/Structure/Structure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Strudture
+{
+    public struct First
+    {
+        public int i;
+
+        public void Display()
+        {
+            Console.WriteLine("Method is called in Structure " + i);
+        }
+        public First(int i) {
+            this.i = i;
+        }
+        static void Main() {
+            //without instance creation 
+            First f1;
+            f1.i = 10;
+            f1.Display();
+            //with instance creation
+            First f2= new First();
+            f2.Display();
+            //parameterized constructor
+            First f3=new First(21);
+            f3.Display();
+            Console.ReadLine();
+            
+        }
+    }
+}
